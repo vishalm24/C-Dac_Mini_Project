27fd654c51c69c232a64a85119f735f72d2b147f
vishalm24 <122714197+vishalm24@users.noreply.github.com>
2023-02-24 11:14:33 +0530

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASPNETMVCCRUD.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
